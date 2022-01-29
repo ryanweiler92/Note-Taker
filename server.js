714b41793f5e0191212d33d25d2f46017b4987eb
ryanweiler92 <ryanweiler92@gmail.com>
2022-01-29 11:53:34 -0500

    validating post data

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
+const fs = require('fs');
+const path = require('path')
 const { notes } = require('./data/db')
-
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -26,10 +27,23 @@ function findById(id, notesArray) {
 };
 
 function createNewNote(body, notesArray) {
-    console.log(body)
+    const note = body;
+    notesArray.push(note)
+    fs.writeFileSync(
+        path.join(__dirname, './data/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+};
 
-
-    return body;
+function validateNote(note) {
+    if (!note.title || typeof note.title !== 'string') {
+        return false
+    }
+    if (!note.text || typeof note.text !== 'string') {
+        return false
+    }
+    return true;
 }
 
 app.get('/api/db', (req, res) => {
@@ -51,8 +65,13 @@ app.get('/api/db/:id', (req, res) => {
 
 app.post('/api/db', (req, res) => {
     req.body.id = notes.length.toString();
-    
-    res.json(req.body)
+
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.');
+    } else {
+        const note = createNewNote(req.body, notes)
+        res.json(note)
+    }
 });
 
 app.listen(PORT, () => {
