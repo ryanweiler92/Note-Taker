52cdabde7a729b2267c0f894575a9614dbc62d04
ryanweiler92 <ryanweiler92@gmail.com>
2022-01-29 11:17:08 -0500

    get api data functions

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ function filterByQuery(query, notesArray) {
         filteredResults = filteredResults.filter(note => note.text === query.text)
     }
     return filteredResults
+};
+
+function findById(id, notesArray) {
+    const result = notesArray.filter(note => note.id === id)[0];
+    return result
 }
 
 app.get('/api/db', (req, res) => {
@@ -24,6 +29,15 @@ app.get('/api/db', (req, res) => {
         results = filterByQuery(req.query, results)
     }
     res.json(results)
+});
+
+app.get('/api/db/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+    res.json(result)
+    } else {
+        res.send(404)
+    }
 })
 
 app.listen(PORT, () => {
